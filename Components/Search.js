4198e10cb6c03bcbbf428431b5c80fc6f48b27e8
Hugo Lausenaz-Pire <==>
2020-07-17 15:03:27 +0200

    Les faire avec Style !

---
+++ Components/Search.js
@@ -0,0 +1,27 @@
+//Components/Search.js
+import React from 'react'
+import { StyleSheet, View, TextInput, Button } from 'react-native'
+
+
+class Search extends React.Component {
+  render() {
+    return(
+      <View style={{ marginTop: 60}}>
+        <TextInput style={styles.textinput} placeholder='Titre du film'/>
+        <Button  title='Rechercher' onPress={() => {}}/>
+      </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  }
+})
+
+export default Search
